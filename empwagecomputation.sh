afb023696ac07a568e0e6fdec9d576df2334a9e4
Ankitapatil <ankitakhairnar10@gmail.com>
2021-02-28 14:18:50 +0530

    Added: calculate wages total working Hrs for a month

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -5,29 +5,33 @@ IS_FULLTIME=1
 IS_PARTTIME=2
 EMP_RATE_PER_HR=20
 NUM_WORKING_DAYS=20
+MAX_HRS_IN_MONTH=100
 
 #variable
-tatalsalary=0
 
-for ((day=1; day<=$NUM_WORKING_DAYS; day++))
+totalworkingdays=0
+totalEmpHrs=0
+
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalworkingdays -lt $NUM_WORKING_DAYS ]]
 do
+  ((totalworkingdays++))
   empcheck=$((RANDOM%3))
   case  $empcheck in
-            $IS_FULLTIME)
-             emphrs=8
-             ;;
-          $IS_PARTTIME)
-              emphrs=4
+     $IS_FULLTIME)
+        emphrs=8
+   ;;
+    $IS_PARTTIME)
+  emphrs=4
 
-             ;;
-             *)
-              emphrs=0
-             ;;
-   esac
-         salary=$(($emphrs*$EMP_RATE_PER_HR))
-         totalsalary=$(($totalsalary+$salary))
-         echo $totalsalary
- done
+   ;;
+   *)
+     emphrs=0
+    ;;
+  esac
+totalEmpHrs=$(($totalEmpHrs+$emphrs))
 
-
+done
+totalsalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
+echo $totalsalary
 
